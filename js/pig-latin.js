e101d173155ce80c0f21c65c0c365f468cb7da59
Connor Hansen <chansen13@georgefox.edu>
2021-08-18 15:15:41 -0700

    add full functionality for most cases. Known bug: scrambles punctuation.

--- js/pig-latin.js
+++ js/pig-latin.js
@@ -1,36 +1,89 @@
-let words = ["dog", "cat", "rat"];
-const vowels = ["a", "e", "i", "o", "u"];
-let newWords = [];
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    var wordString = $("#text-passage").val().trim();
+    var words = wordString.split(" ");
+    let newWords = [];
+    showTranslation(pigLatin(words, newWords));
+  });
+});
 
-
-function pigLatin(words, newWords) {
-  for (var i=0; i < words.length; i++) {
-    var word = words[i] + "way";
-    newWords.push(word);
-  }
-  return newWords;
+function showTranslation(translated) {
+  $("#translation").html(translated);
 }
 
-pigLatin(words, newWords);
+function pigLatin(words, newWords) {
+  for (var i = 0; i < words.length; i++) {
+
+    var word = words[i];
+    var newEnd;
+    var firstChar = word.charAt(0);
+
+    if (!isLetter(firstChar)) {
+      newEnd = firstChar;
+      word = word.slice(1);
+    }
+
+    // If the word starts with a vowel, add 'way'
+    if (checkVowel(firstChar)) {
+      newEnd = "way";
+    }
+
+    // If the word starts with a consonant,
+    // take the first letter, add to the end + 'ay'
+    // -- add a check for 'qu', add 'quay'
+    if (checkQ(firstChar) && (word.charAt(1) === "u")) {
+      newEnd = firstChar + word.charAt(1) + "ay";
+      word = word.slice(2);
+    }
+    else if (checkCons(firstChar)) {
+        newEnd = firstChar + "ay";
+        word = word.slice(1);
+      }
+    
+    word.toString();
+    word = word + newEnd;
+    newWords.push(word);
+  }
+  var translated = newWords.join(" ");
+  console.log(translated);
+  return translated;
+  // return newWords;
+}
+
+// pigLatin(words, newWords);
 
 
-
-
-
-
-
-
-function checkVowel(text) {
-  if (text === 'a') {
-  return true;
+function checkVowel(char) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(char.toLowerCase())) {
+    return true;
   }
   else {
     return false;
   }
 }
 
-function pigLatin(word) {
-  if (checkVowel(word[0])) {
-    return word + 'way';
+function checkCons(char) {
+  const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  if (consonants.includes(char.toLowerCase())) {
+    return true;
   }
+  else {
+    return false;
+  }
+}
+
+function checkQ(char) {
+  const isQ = ["q"];
+  if (isQ.includes(char)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function isLetter(char) {
+  return char.length === 1 && char.match(/[a-z]/i);
 }
